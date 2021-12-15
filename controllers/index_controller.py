e2af617b803152c06a72f664ebe71fec8e9b8ed8
kn329 <90405677+kn329@users.noreply.github.com>
2021-12-15 15:28:45 -0500

    Delete controllers directory

--- controllers/index_controller.py
+++
@@ -1,9 +0,0 @@
-from controllers.controller import ControllerBase
-from flask import render_template
-
-class IndexController(ControllerBase):
-    @staticmethod
-    def get():
-        return render_template('index.html')
-
-
